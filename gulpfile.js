c087e946d3b5ee00d52836f8e257d77e6dcce8c3
Christian Taepper <ct@tresmo.de>
2015-05-26 17:14:55 +0200

    add sequences

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 
 var pjson = require('./package.json');
 var gulp = require('gulp');
+var run = require('run-sequence');
 gulp.utils = require('./gulp/utils');
 
 require('./gulp/tasks')(gulp, {gulp: {}}, pjson);
+
+gulp.task('release', function (cb) {
+	run('release.version', 'npm.publish', cb);
+});
